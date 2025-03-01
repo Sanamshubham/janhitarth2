48df5bbc9469765a27f60437e00bdaaa8a7cd111
Shubham Shrivastav <118472604+Sanamshubham@users.noreply.github.com>
2025-03-01 12:51:41 +0530

    ff
    ff

---
+++ nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+    <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <img src="img/logo.JPG" alt="Janhitath Helping Hand Foundation Logo" style="max-height: 40px;" class="me-3">
+        <h2 class="m-0 text-primary">Janhitarth </h2>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="index.html" class="nav-item nav-link active">Home</a>
+            <a href="about.html" class="nav-item nav-link">About Us</a>
+            <a href="projects.html" class="nav-item nav-link">Our Work</a>
+            <a href="get-involved.html" class="nav-item nav-link">Get Involved</a>
+            <a href="donate.html" class="nav-item nav-link">Donate</a>
+            <a href="events.html" class="nav-item nav-link">Events</a>
+           <!-- <a href="impact.html" class="nav-item nav-link">Impact</a>-->
+            <a href="blog.html" class="nav-item nav-link">Blog</a>
+            <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+        </div>
+        <a href="donate.html" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Donate Now<i class="fa fa-arrow-right ms-3"></i></a>
+    </div>
+</nav>
